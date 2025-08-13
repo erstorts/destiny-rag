37ebeb61d68478d651ab1204eccdbcbdc7c3841c
erstorts <me@emmettstorts.com>
2025-08-13 17:36:11 -0600

    update vector db

--- streamlit-chat.py
+++ streamlit-chat.py
@@ -1,10 +1,16 @@
 from openai import OpenAI
 import streamlit as st
 from pinecone import Pinecone
+import segment.analytics as analytics
+import json
 
+analytics.write_key = st.secrets["SEGMENT_API_KEY"]
 pc = Pinecone()
 client = OpenAI()
-index_name = "quickstart-py"
+
+analytics.page('null', name="whitman-chat")
+
+index_name = "whitman"
 dense_index = pc.Index(index_name)
 
 system_prompt = """You are a helpful librarian's assistant. 
@@ -15,7 +21,7 @@ num_results = st.sidebar.slider("Number of results", min_value=1, max_value=10, 
 
 def search_pinecone(query):
     reranked_results = dense_index.search(
-        namespace="example-namespace",
+        namespace="whitman",
         query={
             "top_k": num_results,
             "inputs": {
@@ -28,7 +34,12 @@ def search_pinecone(query):
             "rank_fields": ["synopsis"]
         }   
     )
-
+    
+    for hit in reranked_results['result']['hits']:
+        hit_dict = hit.to_dict()
+        json_hit = json.dumps(hit_dict)
+        analytics.track('null', event="search_result", properties={"search_result": json_hit})
+    
     return reranked_results['result']['hits']
 
 
@@ -55,6 +66,8 @@ if user_input := st.chat_input():
     The title should be bolded.
     The author should be in italics.
     The summary should be in regular text.
+    The '_score' is the relevance score of the book to the question.
+    The '_id' is the ISBN of the book.
     You response should include {} number of books.
     You should start your response with something like 'Here is the information I found: '""".format(search_results, user_input, num_results)
 
@@ -62,12 +75,14 @@ if user_input := st.chat_input():
     # Add user's actual question to display messages
     st.session_state.display_messages.append({"role": "user", "content": user_input})
     st.chat_message("user").write(user_input)
+    analytics.track('null',event="user_input", properties={"user_input": user_input})
     
     # Add full prompt with search results to context messages for the model
     st.session_state.context_messages.append({"role": "user", "content": prompt})
     with st.status("Thinking..."):
         response = client.responses.create(model="gpt-3.5-turbo", input=st.session_state.context_messages, instructions=system_prompt)
         msg = response.output_text
+        analytics.track('null', event="assistant_response", properties={"assistant_response": msg})
     
     # Add assistant response to both display and context messages
     st.session_state.display_messages.append({"role": "assistant", "content": msg})
